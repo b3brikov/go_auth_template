287179a6e03591a870d7e5a97aa0d37fb050d15f
bebrikov <ksasa9359@gmail.com>
2025-12-12 01:22:02 +0300

    improved config

--- app/internal/config/config.go
+++ app/internal/config/config.go
@@ -1,36 +1,26 @@
 package config
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"time"
-
-	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	Env          string        `yaml:"env" env-default:"local"`
-	TokenTTL     time.Duration `yaml:"token_ttl" env-required:"true"`
-	RedisAddr    string        `yaml:"addr_redis" env-required:"true"`
+	TokenTTL     time.Duration
+	RedisAddr    string
 	Storage_path string
 }
 
 func MustLoad() *Config {
-	path := fetchConfigPath()
-	if path == "" {
-		panic("config path is empty")
-	}
-
-	if _, err := os.Stat(path); err != nil {
-		panic("config file does not exist: " + path)
-	}
-
 	var cfg Config
-
-	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
-		panic("failed read config" + err.Error())
+	cfg.RedisAddr = os.Getenv("REDIS_ADDR")
+	Dur, err := time.ParseDuration(os.Getenv("TOKEN_TTL"))
+	if err != nil {
+		panic(err.Error())
 	}
+	cfg.TokenTTL = Dur
+
 	cfg.Storage_path = fmt.Sprintf(
 		"postgres://%s:%s@postgres:5432/%s?sslmode=disable",
 		os.Getenv("POSTGRES_USER"),
@@ -39,17 +29,4 @@ func MustLoad() *Config {
 	)
 
 	return &cfg
-}
-
-func fetchConfigPath() string {
-	var res string
-
-	flag.StringVar(&res, "config", "", "path to config file")
-	flag.Parse()
-
-	if res == "" {
-		res = os.Getenv("CONFIG_PATH")
-	}
-
-	return res
 }
